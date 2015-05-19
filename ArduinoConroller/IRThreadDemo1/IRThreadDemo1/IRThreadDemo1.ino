4e29bc3fefc33bd734e78762ef6b1cce66f2cd41
Michael Wimble <mike@webself.com>
2015-05-18 21:08:31 -0700

    Odometer, IR sensors working on 10ms thread.

--- ArduinoConroller/IRThreadDemo1/IRThreadDemo1/IRThreadDemo1.ino
+++ ArduinoConroller/IRThreadDemo1/IRThreadDemo1/IRThreadDemo1.ino
@@ -11,11 +11,37 @@ LineSensorThread* lineSensorThread;
 QuadratureEncoderThread* quadratureEncoderThread;
 ThreadController controller = ThreadController();
 
+#define MAP_ROWS 160
+#define CLICKS_PER_ROW 5
+#define SENSOR_COLS 8
+
+int mapP[MAP_ROWS][8] = { 0 };
+
 ISR(timer5Event) {
   // 10ms timer
   resetTimer5();
   controller.run();
+  int counterRow = quadratureEncoderThread->lastCounter / CLICKS_PER_ROW;
+  
+  if (counterRow < 0) { counterRow = 0; }
+  else if (counterRow >= MAP_ROWS) { counterRow = MAP_ROWS -1; }
+  
+  for (int col = 0; col < SENSOR_COLS; col++) {
+    int lrCol = (SENSOR_COLS - col) - 1;
+    if ((counterRow < 0) || (counterRow >= MAP_ROWS)) {
+     Serial.print("counterRow range error: ");
+     Serial.println(counterRow);
+    } else {
+      if ((lrCol < 0) || (lrCol >= 8)) {
+        Serial.print("lrCol range error: ");
+        Serial.println(lrCol);
+      } else {
+        mapP[counterRow][lrCol] = lineSensorThread->sensorValues[col];
+      }
+    }
+  }
 }
+
 
 void setup() {
   Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
@@ -27,26 +53,49 @@ void setup() {
   controller.add(quadratureEncoderThread);
   Serial.println("setup end");
   delay(1000);
-  startTimer5(10000);
+  startTimer5(5000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(500);
-  Serial.print("[");Serial.print(lineSensorThread->readCount);Serial.print("] ");
-  Serial.print("position: ");
-  Serial.print(lineSensorThread->position);
-  Serial.print(" -- ");
-  for (int i = 0; i < 8; i++) {
-    Serial.print(" ");
-    Serial.print(lineSensorThread->sensorValues[i]);
+  Serial.println("-- Start mapping");
+  while ((quadratureEncoderThread->lastCounter / CLICKS_PER_ROW) < MAP_ROWS) {
+    Serial.print("Counter: ");
+    Serial.println(quadratureEncoderThread->lastCounter);
+    delay(500);
   }
-
-  Serial.println();
   
-  Serial.print("Counter: ");
-  Serial.println(quadratureEncoderThread->lastCounter);
-
-  Serial.println();  
+  Serial.println("-- End mapping");
+  
+  for (int mapx = MAP_ROWS - 1; mapx >= 0; mapx--) {
+    Serial.print("[");
+    Serial.print(mapx);
+    Serial.print("]\t");
+    for (int colx = 0; colx < SENSOR_COLS; colx++) {
+      Serial.print(mapP[mapx][colx]);
+      Serial.print("\t");
+    }
+    
+    Serial.println();
+  }
+  
+  Serial.println("-- END --");
+  
+  for(;;) { delay(500); }
+//  Serial.print("[");Serial.print(lineSensorThread->readCount);Serial.print("] ");
+//  Serial.print("position: ");
+//  Serial.print(lineSensorThread->position);
+//  Serial.print(" -- ");
+//  for (int i = 0; i < 8; i++) {
+//    Serial.print(" ");
+//    Serial.print(lineSensorThread->sensorValues[i]);
+//  }
+//
+//  Serial.println();
+//  
+//  Serial.print("Counter: ");
+//  Serial.println(quadratureEncoderThread->lastCounter);
+//
+//  Serial.println();  
 }
 
