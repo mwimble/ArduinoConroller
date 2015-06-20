747ba2a715d23bcb08a3c64b32b3cedc347e4206
Michael Wimble <mike@webself.com>
2015-06-20 13:22:32 -0700

    Add call to motor.FORWARD at end of FOLLOW_LINE strategy. FIND_LINE_END strategy to stop when odo exceeds 700 instead of 200.

--- ArduinoConroller/ControllerWithTimer/Strategy.h
+++ ArduinoConroller/ControllerWithTimer/Strategy.h
@@ -7,16 +7,23 @@ extern Motor motor;
 
 class Strategy {
   public:
-  
+
+  typedef enum TState {
+    FOLLOW_LINE,
+    FIND_LINE_END,
+    FOUND_END,
+    STOP,
+  };
+    
   Strategy() :
     a_speed_(64),
     b_speed_(64),
-    state(FOLLOW_LINE) {
+    state_(FOLLOW_LINE) {
   }
   
   void Initialize() {
     motor.Stop();
-    state = FOLLOW_LINE;
+    state_ = FOLLOW_LINE;
     motor.Forward(a_speed_, b_speed_);
   }
   
@@ -28,16 +35,18 @@ class Strategy {
     Serial.print(quadratureEncoder.Counter());
     Serial.print("] ");
     
-    switch (state) {
+    switch (state_) {
       case FOLLOW_LINE:
         Serial.println("+++ FOLLOW_LINE");
         if (leftTurnFound || rightTurnFound) {
-          lineStartOdo = quadratureEncoder.Counter();
-          Serial.print("... ... FOUND start of crossing, left: ");
+          lineStartOdo_ = quadratureEncoder.Counter();
+          Serial.print(">>> FOUND start of crossing, odo: ");
+          Serial.print(lineStartOdo_);
+          Serial.print(", left: ");
           Serial.print(leftTurnFound);
           Serial.print(", right: ");
           Serial.println(rightTurnFound);
-          state = FIND_LINE_END;
+          state_ = FIND_LINE_END;
         } else { // Do PID.
           if (position < 3.0) {
             a_speed_ -= 20;
@@ -61,7 +70,7 @@ class Strategy {
             Serial.print(a_speed_);
             Serial.print(", b_speed: ");
             Serial.println(b_speed_);
-            state = STOP;
+            state_ = STOP;
           } else {
             Serial.print("... ... PID. Position: ");
             Serial.print(position);
@@ -74,16 +83,24 @@ class Strategy {
           */
         }
                     
+        motor.Forward(a_speed_, b_speed_);
         break;
         
       case FIND_LINE_END:
         Serial.println("+++ FOLLOW_LINE_END");
-        if (quadratureEncoder.Counter() > (lineStartOdo + 200)) {
-          state = FOUND_END;
+        if (quadratureEncoder.Counter() > (lineStartOdo_ + 700)) {
+          state_ = FOUND_END;
           motor.Stop();
         } else if (!leftTurnFound && !rightTurnFound) {
-          lineEndOdo = quadratureEncoder.Counter();
-          state = FOUND_END; //#####
+          lineEndOdo_ = quadratureEncoder.Counter();
+          Serial.print(">>> Found line end Odo:");
+          Serial.print(lineEndOdo_);
+          Serial.print(", width: ");
+          Serial.print(lineEndOdo_ - lineStartOdo_);
+          Serial.print(" Odos or ");
+          Serial.print((lineEndOdo_ - lineStartOdo_) / 0.0067);
+          Serial.println(" IN");
+          state_ = FOUND_END; //#####
         }
         
         break;
@@ -101,20 +118,13 @@ class Strategy {
   }
   
   private:
-  typedef enum State {
-    FOLLOW_LINE,
-    FIND_LINE_END,
-    FOUND_END,
-    STOP,
-  };
-  
   const LineSensor::TSensorArray& lineSensorValues_ = lineSensor.SensorValues();
   int a_speed_;
   int b_speed_;
   float heading_;
-  long lineEndOdo;
-  long lineStartOdo;
-  State state;
+  long lineEndOdo_;
+  long lineStartOdo_;
+  TState state_;
 };
 
 #endif
